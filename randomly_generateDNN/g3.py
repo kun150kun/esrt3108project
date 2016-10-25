5beb281bf4db22cb798d4baceea5263869e152c4
kun150kun <kunpinghuang@kunpinghuangs-MacBook-Pro.local>
2016-10-25 21:47:15 +0800

    gwith lost

--- randomly_generateDNN/g3.py
+++ randomly_generateDNN/g3.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import copy
 import random as rd
 import numpy as np
+import time
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -27,7 +28,7 @@ def re_x_size(x_size,strides,padding):
                 x_size[i] = x_size[i] / s[i] + 1
     return x_size
 
-def conv2d(inputs,num_filters_in,num_filters_out,weights_size,strides=[1,1,1,1],padding='SAME'):
+def conv2d(inputs,num_filters_in,num_filters_out,weights_size,num_parameter,strides=[1,1,1,1],padding='SAME'):
     weights_shape = [weights_size[0],weights_size[1],
                      num_filters_in,num_filters_out]
     weights = weight_variable(weights_shape)
@@ -35,7 +36,8 @@ def conv2d(inputs,num_filters_in,num_filters_out,weights_size,strides=[1,1,1,1],
     biases = bias_variable(bias_shape)
     conv = tf.nn.conv2d(inputs, weights, strides=strides,padding=padding)
     output = tf.nn.bias_add(conv, biases)
-    return output
+    num_parameter += num_filters_in * num_filters_out * weights_size[0] * weights_size[1]
+    return output,num_parameter
 
 def max_pool(inputs, k_size=[1,2,2,1], strides = [1,2,2,1], padding='SAME'):
     return tf.nn.max_pool(inputs,ksize=k_size, strides = strides, padding=padding)
@@ -43,68 +45,68 @@ def max_pool(inputs, k_size=[1,2,2,1], strides = [1,2,2,1], padding='SAME'):
 def avg_pool(inputs, k_size=[1,2,2,1],strides=[1,2,2,1],padding='SAME'):
     return tf.nn.avg_pool(inputs,ksize=k_size,strides=strides,padding=padding)
 
-def conv1_layer(inputs,num_filters_in,x_size):
+def conv1_layer(inputs,num_filters_in,x_size,num_parameter):
     if num_filters_in == 1:
-        num_filters_out = 32
+        num_filters_out = 16
     else:
         num_filters_out = num_filters_in
-    h = conv2d(inputs,num_filters_in,num_filters_out,[3,3])
+    h,num_parameter = conv2d(inputs,num_filters_in,num_filters_out,[3,3],num_parameter)
     output = tf.nn.relu(h)
     x_size = re_x_size(x_size,[1,1,1,1],padding='SAME')
-    return output,x_size,num_filters_out
+    return output,x_size,num_filters_out,num_parameter
 
-def conv2_layer(inputs,num_filters_in,x_size):
+def conv2_layer(inputs,num_filters_in,x_size,num_parameter):
     if num_filters_in == 1:
-        num_filters_out = 32
+        num_filters_out = 16
     else:
-        num_filters_out = 32 + num_filters_in
-    h = conv2d(inputs,num_filters_in,num_filters_out,[3,3],[1,2,2,1])
+        num_filters_out = 16 + num_filters_in
+    h,num_parameter = conv2d(inputs,num_filters_in,num_filters_out,[3,3],num_parameter,[1,2,2,1])
     output = tf.nn.relu(h)
     x_size = re_x_size(x_size,[1,2,2,1],padding='SAME')
-    return output,x_size,num_filters_out
+    return output,x_size,num_filters_out,num_parameter
 
-def pool1_layer(inputs,num_filters_in,x_size):
+def pool1_layer(inputs,num_filters_in,x_size,num_parameter):
     outputs = max_pool(inputs)
     x_size = re_x_size(x_size,[1,2,2,1],padding='SAME')
-    return outputs,x_size,num_filters_in
+    return outputs,x_size,num_filters_in,num_parameter
 
-def pool2_layer(inputs,num_filters_in,x_size):
+def pool2_layer(inputs,num_filters_in,x_size,num_parameter):
     outputs = avg_pool(inputs)
     x_size = re_x_size(x_size,[1,2,2,1],padding='SAME')
-    return outputs,x_size,num_filters_in
+    return outputs,x_size,num_filters_in,num_parameter
 
-def inception1_layer(inputs,num_filters_in,x_size):
+def inception1_layer(inputs,num_filters_in,x_size,num_parameter):
     if num_filters_in == 1:
-        num_filters_out = 32
+        num_filters_out = 16
     else:
         num_filters_out = num_filters_in
-    branch11 = conv2d(inputs,num_filters_in,num_filters_out,[1,1])
-    branch33 = conv2d(inputs,num_filters_in,num_filters_out/4*3,[1,1])
-    branch33 = conv2d(branch33,num_filters_out/4*3,num_filters_out,[3,3])
+    branch11,num_parameter = conv2d(inputs,num_filters_in,num_filters_out,[1,1],num_parameter)
+    branch33,num_parameter = conv2d(inputs,num_filters_in,num_filters_out/4*3,[1,1],num_parameter)
+    branch33,num_parameter = conv2d(branch33,num_filters_out/4*3,num_filters_out,[3,3],num_parameter)
     branch_pool = avg_pool(inputs,[1,2,2,1],[1,1,1,1])
-    branch_pool = conv2d(branch_pool,num_filters_in,num_filters_out,[1,1])
+    branch_pool,num_parameter = conv2d(branch_pool,num_filters_in,num_filters_out,[1,1],num_parameter)
     output = tf.concat(3,[branch11,branch33,branch_pool])
-    return output,x_size,3*num_filters_out
+    return output,x_size,3*num_filters_out,num_parameter
 
-def inception2_layer(inputs,num_filters_in,x_size):
+def inception2_layer(inputs,num_filters_in,x_size,num_parameter):
     if num_filters_in == 1:
-        num_filters_out = 32
+        num_filters_out = 16
     else:
-        num_filters_out = num_filters_in + 32
-    branch11 = conv2d(inputs,num_filters_in,num_filters_out,[1,1])
-    branch55 = conv2d(inputs,num_filters_in,num_filters_out/4*3,[1,1])
-    branch55 = conv2d(branch55,num_filters_out/4*3,num_filters_out,[1,5])
-    branch55 = conv2d(branch55,num_filters_out,num_filters_out,[5,1])
+        num_filters_out = num_filters_in + 16
+    branch11,num_parameter = conv2d(inputs,num_filters_in,num_filters_out,[1,1],num_parameter)
+    branch33,num_parameter = conv2d(inputs,num_filters_in,num_filters_out/4*3,[1,1],num_parameter)
+    branch33,num_parameter = conv2d(branch33,num_filters_out/4*3,num_filters_out,[1,3],num_parameter)
+    branch33,num_parameter = conv2d(branch33,num_filters_out,num_filters_out,[3,1],num_parameter)
     branch_pool = avg_pool(inputs,[1,2,2,1],[1,1,1,1])
-    branch_pool = conv2d(branch_pool,num_filters_in,num_filters_out,[1,1])
-    output = tf.concat(3,[branch11,branch55,branch_pool])
-    return output,x_size,3*num_filters_out
+    branch_pool,num_parameter = conv2d(branch_pool,num_filters_in,num_filters_out,[1,1],num_parameter)
+    output = tf.concat(3,[branch11,branch33,branch_pool])
+    return output,x_size,3*num_filters_out,num_parameter
 
-def output(h,x_size,depth,keep_prob):
+def output(h,x_size,depth,keep_prob,num_parameter):
     x_dim=x_size[0] * x_size[1] * depth
     W = weight_variable([x_dim,1024])
     b = bias_variable([1024])
-    
+    num_parameter += x_dim * 1024 + 1024
     h_flat = tf.reshape(h, [-1, x_dim])
     h1 = tf.nn.relu(tf.matmul(h_flat, W) + b)
     #drop out.
@@ -112,9 +114,9 @@ def output(h,x_size,depth,keep_prob):
     
     W2 = weight_variable([1024, 10])
     b2 = bias_variable([10])
-    
+    num_parameter += 1024*10 + 10
     logits = tf.matmul(h_drop, W2) + b2
-    return logits
+    return logits,num_parameter
 
 def can_train(x_size,i):
     if i == 6 and x_size[0] >=5 and x_size[1]>=5:
@@ -131,10 +133,11 @@ x = tf.placeholder(tf.float32, shape=[None, 28*28])
 labels = tf.placeholder(tf.float32, shape=[None,10])
 
 dnn = list()
-for j in range(20):
-    len = rd.randint(2,5)
+lost = list()
+dnn.append([1,1,3,2,4])
+for j in range(1,20):
     k = list()
-    for i in range(len):
+    for i in range(5):
         k.append(rd.randint(1,6))
     dnn.append(copy.deepcopy(k))
 print dnn
@@ -151,18 +154,20 @@ for k in range(20):
     h = x_image
     x_size = [28,28]
     channel = 1
+    num_parameter = 0
     for i in dnn[k]:
         if can_train(x_size,i) == 1:
-            h,x_size,channel = layer_f[i](h,channel,x_size)
+            h,x_size,channel,num_parameter = layer_f[i](h,channel,x_size,num_parameter)
         else:
             nottrain = 1
 
     if nottrain == 1:
         print "not train"
+        lost.append([])
         continue
     print x_size
     keep_prob = tf.placeholder(tf.float32)
-    logits = output(h,x_size,channel,keep_prob)
+    logits,num_parameter = output(h,x_size,channel,keep_prob,num_parameter)
 
     sess.run(tf.initialize_all_variables())
     
@@ -173,17 +178,19 @@ for k in range(20):
     sess.run(tf.initialize_all_variables())
     duration = 0
     acc=list()
-    for i in range(1000):
+    for i in range(1,1000):
         batch = mnist.train.next_batch(batch_size)
         if i%100 == 0:
             train_accuracy = accuracy.eval(feed_dict={
                                            x:batch[0], labels: batch[1], keep_prob: 1.0})
             print("step %d, training accuracy %g"%(i, train_accuracy))
-            acc.append(train_accuracy)
+            acc.append(train_accuracy*100)
         start_time = time.time()
         train_step.run(feed_dict={x: batch[0], labels: batch[1], keep_prob: 0.5})
         duration += time.time() - start_time
-    acc = acc[1:length(acc)]
     variance=np.var(acc)
-    print("test accuracy %g"%accuracy.eval(feed_dict={
-                                           x: mnist.test.images, labels: mnist.test.labels, keep_prob: 1.0}))
+    test_accuracy = accuracy.eval(feed_dict={
+                             x: mnist.test.images, labels: mnist.test.labels, keep_prob: 1.0})
+    print("%g"%test_accuracy)
+    lost.append([test_accuracy,duration,num_parameter,variance])
+    print lost[k]
