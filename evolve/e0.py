9674983b38c9f18bfd971c94fefda57b53712add
kun150kun <kunpinghuang@kunpinghuangs-MacBook-Pro.local>
2016-11-11 00:19:39 +0800

    e
    e

--- evolve/e0.py
+++ evolve/e0.py
@@ -18,17 +18,18 @@ loss_sequence = ga.initialisation(loss_sequence,10,5,5)
 x_image = tf.reshape(x, [-1,28,28,1])
 sess = tf.Session()
 
-losslen = len(loss_sequence)
+
 for le in range(20):
     dnn_init = list()
     loss_fitness = list()
     dnn_init.append([1,3,1,2])
     dnn_init = ga.initialisation(dnn_init,5,4,4)
+    losslen = len(loss_sequence)
     for lstep in range(losslen):
-        print(loss_sequence[lstep])
+        print("loss_se",lstep,loss_sequence[lstep])
         dnn = copy.deepcopy(dnn_init)
         loss_fitness = train.dnn_evolve_train(dnn,mnist,x_image,loss_sequence[lstep],loss_fitness,
                                               x,labels,sess,5)
         print("loss_fitness",loss_fitness[lstep])
     if le != 20:
-        loss_sequence = ga.loss_add(loss_sequence,loss_fitness,10,6)
+        loss_sequence = ga.loss_add(loss_sequence,loss_fitness,10,5)
